331e39d9bb7a10ee62545907eaf6d6c9de318f75
Pamela McA'Nulty <pamela@webreply.com>
2012-06-06 13:20:08 -0400

    Get fabadmin working and looking better
    o Handle @task tasks
    o Improve/re-layout the task_list page
    o Survive tasks w/o arguments
    o Handle tasks imported by the fabfile (e.g. submodule.task_name)
    
    Signed-off-by: Pamela McA'Nulty <pamela@webreply.com>

--- fabadmin/views.py
+++ fabadmin/views.py
@@ -1,5 +1,6 @@
 import inspect
 import os
+import pprint
 
 from ansi2html import Ansi2HTMLConverter
 from django.conf import settings
@@ -8,22 +9,38 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.datastructures import SortedDict
 from fabric.main import load_tasks_from_module
+from fabric.tasks import WrappedCallableTask
 
 from fabadmin.forms import FabfileForm
 import subprocess
 
 
+import inspect
+
+def getargspec(func):
+    if isinstance(func, WrappedCallableTask):
+        if hasattr(func, "wrapped"):
+            func = func.wrapped
+        else:
+            func = func.__call__
+    return inspect.getargspec(func)
+    
 class Task(object):
     """Class that summarizes a fabric task."""
 
-    def __init__(self, func):
-        argspec = inspect.getargspec(func)
+    def __init__(self, func, parent_name=None):
+        argspec = getargspec(func)
         defaults = argspec.defaults
         args = argspec.args
         has_args = bool(len(args))
-        docstring = func.__doc__.strip()
+        if func.__doc__:
+            docstring = func.__doc__.strip()
+        else:
+            docstring = ""
         first_newline = docstring.find("\n")
         name = func.__name__
+        if parent_name:
+            name = "%s.%s" % (parent_name, name)
         if first_newline == -1:
             short_docstring = docstring
         else:
@@ -73,20 +90,40 @@ def task_list(request):
     module = os.path.splitext(filename)[0]
     fabfile = __import__(module)
     _, new_style, classic, default = load_tasks_from_module(fabfile)
-    tasks = []
-    for task_group in [new_style, classic, default]:
+    tasks = {}
+    def _add_group(task_group, group_name):
         if task_group:
-            for task in task_group.values():
-                tasks.append(Task(task))
+            for name, task in task_group.items():
+                if isinstance(task, dict):
+                    if group_name:
+                        name = "%s.%s" % (group_name, name)
+                    _add_group(task, name)
+                else:
+                    t = Task(task, group_name)
+                    if not t.name in tasks:
+                        print "adding task: %s" % t.name
+                        tasks[t.name] = t
+                    else:
+                        print "skipping task: %s" % t.name
+    
+    for task_group in [new_style, classic, default]:
+        _add_group(new_style, None)
+    
+    task_names = tasks.keys()
+    task_names.sort()
+    tasks = [tasks[n] for n in task_names]
+    
     task_choices = [NullTask()] + tasks
     output = ""
     if request.method == "POST":
         fabfile_form = FabfileForm(request.POST, tasks=task_choices)
         if fabfile_form.is_valid():
             cleaned_data = fabfile_form.cleaned_data
-            task = cleaned_data['task']
+            task_str = cleaned_data['task']
             arguments = cleaned_data['arguments']
-            args = ['fab', '-f', fabfile_path, task + ":" + arguments]
+            if arguments:
+                task_str = "%s:%s" % (task_str, arguments)
+            args = ['fab', '-f', fabfile_path, task_str]
             process = subprocess.Popen(
                 args, shell=False, stdout=subprocess.PIPE
             )
